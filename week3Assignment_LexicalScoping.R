bb98b0de9a0c7f1a0310ab79a60244758bd90a29
Girish Nautiyal <girishnautiyal91@gmail.com>
2016-10-30 21:57:21 +0530

    Added morre comments to enhance understandability

--- week3Assignment_LexicalScoping.R
+++ week3Assignment_LexicalScoping.R
@@ -1,24 +1,30 @@
+# makeCacheMatrix function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x1= numeric()) {
-          
+            
+            # stores cached value, if any otherwise holds NULL
             cache <- NULL
             
+            # Store the matrix
             setMatrix <- function(temp_val) {
                         x1<<- temp_val
                         cache <<- NULL
                   }
-      
+            # get/ fetch the matrix
             getMatrix <- function() {
                         x1
                   }
-        
+           
+            # cache the given argument
             cacheInverse <- function(solve) {
                             cache <<- solve
                     }
-          
+           
+            # get the cached value
             getInverse <- function() {
                               cache
                       }
-                    
+           
+            # return a list with all above elements        
             list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
             }
 
